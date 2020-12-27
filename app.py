1d005139b07accf606422ade0aad3b7da758c82d
Gaurav parmar <54779403+thegauravparmar@users.noreply.github.com>
2020-12-28 01:43:21 +0530

    info added

--- app.py
+++ app.py
@@ -4,58 +4,52 @@ from twilio.rest import Client
 import os
 from marketstack import get_stock_price
 
-app = Flask(__name__) 
+app = Flask(__name__)  # initializing flask app
 
 ID = os.environ.get('TWILIO_ACCOUNT')
 TOKEN = os.environ.get('TWILIO_TOKEN')
-client = Client(ID,TOKEN)
+client = Client(ID, TOKEN)
 TWILIO_NUMBER = 'whatsapp:+14155238886'
 
-def send_msg(msg,recipient):
+
+def send_msg(msg, recipient):
     client.messages.create(
-        from_ = TWILIO_NUMBER,
-        body = msg,
-        to = recipient
+        from_=TWILIO_NUMBER,
+        body=msg,
+        to=recipient
     )
 
-def process_msg(msg):
+
+def process_msg(msg):  # default function to send all the responses
     response = ""
-    if msg == "hi":
-        response ="Hello, Welcome to stock market chat bot. Type sym:<stock_symbol> to know the price of stock"
+    if msg == "Hi":
+        response = "Hello, Welcome to stock market chat bot. Type sym:<stock_symbol> to know the price of stock"
     elif 'sym:' in msg:
         data = msg.split(":")
         stock_symbol = data[1]
-        stock_price=get_stock_price(stock_symbol)
-        last_price=stock_price['Last_Price']
+        stock_price = get_stock_price(stock_symbol)
+        last_price = stock_price['Last_Price']
         last_price_str = str(last_price)
-        response="The Stock Pirce of " + stock_symbol + " is: $" + last_price_str
+        response = "The Stock Pirce of " + stock_symbol + " is: $" + last_price_str
     else:
         response = "Please type hi to get started"
     return response
 
 
-
-@app.route("/",methods=["POST"])
+@app.route("/", methods=["POST"])  # default route of the program
 def webhook():
     f = request.form
     msg = f['Body']
     sender = f['From']
     response = process_msg(msg)
-    send_msg(response,sender)
-    return "OK",200
+    send_msg(response, sender)
+    return "OK", 200
 
-#TWILIO_ACCOUNT
-#TWILIO_TOKEN
+# TWILIO_ACCOUNT
+# TWILIO_TOKEN
 
 
-# import client from twilio
-# initialise client
-# write a function to process msg
-# write a function to send messag
-# generate a response
-# check response on whstapp
 # MARKETSTACK_KEY = d5a5825c280a0ab2ad1761dcbde60e89
 
-#AC1456f1794c1d1d2846f997f842fdc094
-#7096a0fe331ddf2da774a9847bea9628
-#AC1456f1794c1d1d2846f997f842fdc094
+# AC1456f1794c1d1d2846f997f842fdc094
+# 7096a0fe331ddf2da774a9847bea9628
--- marketstack.py
+++ marketstack.py
@@ -1,27 +1,27 @@
 #MARKETSTACK_KEY=d5a5825c280a0ab2ad1761dcbde60e89
 
-#0 explore the ms docs and find relevent code
-#1 integrate api
-
 #imports
-
-import os
-import requests
-import json
+import os  #to access environment variable
+import requests   #to call the marketstack api
+import json   #to get the data formated
 
 API_KEY = os.environ.get("MARKETSTACK_KEY")
 BASE_URL = 'http://api.marketstack.com/v1/'
 
-def get_stock_price(stock_symbol):
+def get_stock_price(stock_symbol):   #takes input as stocks name and return last lastest price of the stock
     params = {
-        'access_key': API_KEY
+        'access_key': API_KEY      #stores key in accesskey format
     }
-    end_point="".join([BASE_URL, "tickers/" ,stock_symbol, "/intraday/latest"])
-    api_result = requests.get(end_point, params)
+    end_point="".join([BASE_URL, "tickers/" ,stock_symbol, "/intraday/latest"])  #creats a complete url
+    api_result = requests.get(end_point, params)  #joins url with access key to get the result in json
     print(api_result)
-    json_result = json.loads(api_result.text)
+    json_result = json.loads(api_result.text)   #stores the data in json_result variable
     return{
-        "Last_Price": json_result["last"]
+        "Last_Price": json_result["last"]    #get the data of last keyword from the json format
     }
-result=get_stock_price("AMZN")
-print(result)
+result=get_stock_price("AMZN")  #for testing purposes
+print(result)           #for testing purposes
+
+#sample post request
+
+#http://api.marketstack.com/v1/tickers/AMZN/intraday/latest?access_key=d5a5825c280a0ab2ad1761dcbde60e89
